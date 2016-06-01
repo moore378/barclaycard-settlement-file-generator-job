05b95d87d61d142d4ef06ef0401494a2cc043fdf
Manirith Nuth <manirith.nuth@ipsgroupinc.com>
2016-06-01 13:57:21 -0700

    Add back in the x86 platform since Track.dll is a 32-bit DLL and the Any CPU platform executables running as 64-bit cannot load the DLL. Properly set up the configuration to explicitly delineate x86, Any CPU, and Mixed Platforms.

--- deploy-RTCC.bat
+++ deploy-RTCC.bat
@@ -1,5 +1,5 @@
 
-msbuild TransactionManagement.sln /m
+msbuild TransactionManagement.sln /m /p:"Platform=Mixed Platforms" /p:Configuration=Debug
 if %errorlevel% neq 0 exit /b %errorlevel%
 pushd RTCC\bin\Debug
 make-deploy.py
