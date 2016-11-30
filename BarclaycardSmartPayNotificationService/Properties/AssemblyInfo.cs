c4ed5975f94919c504a09fa13277dde7274d2ffd
Richard Moore <richard.moore@ipsgroupinc.com>
2016-11-30 14:04:42 -0800

    STM-49 Updated build scripts

--- BarclaycardSmartPayNotificationService/Properties/AssemblyInfo.cs
+++ BarclaycardSmartPayNotificationService/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BarclacardSmartPayNotificationService")]
+[assembly: AssemblyTitle("BarclaycardSmartPayNotificationService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BarclacardSmartPayNotificationService")]
+[assembly: AssemblyProduct("BarclaycardSmartPayNotificationService")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
